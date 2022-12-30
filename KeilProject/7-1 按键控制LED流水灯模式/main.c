6b1d04d0b5ad01945ff2a7de3ddc28f2dee72473
CYJEVEN <cyjeven@foxmail.com>
2022-12-30 22:19:00 +0800

    7-1 每隔一秒闪烁

--- KeilProject/7-1 按键控制LED流水灯模式/main.c
+++ KeilProject/7-1 按键控制LED流水灯模式/main.c
@@ -2,12 +2,12 @@
 
 
 void Timer0_Init(){
-
-	TMOD=0x01;
-	TF0=0;
-	TR0=1;
-	TH0=64535/256;	//拿出高八位
-	TL0=64535%256;	//保留低八位
+	TMOD &= 0xF0;		//设置定时器模式
+	TMOD |= 0x01;		//设置定时器模式
+	TL0 = 0x66;		//设置定时初始值
+	TH0 = 0xFC;		//设置定时初始值
+	TF0 = 0;		//清除TF0标志
+	TR0 = 1;		//定时器0开始计时
 	ET0=1;
 	EA=1;
 	PT0=0;
@@ -25,8 +25,17 @@ void main(){
 
 }
 
-
+unsigned int T0Count;
 void Timer0_Rountine() interrupt 1{
-	P2_0=0;
+	TL0 = 0x66;		//设置定时初始值
+	TH0 = 0xFC;		//设置定时初始值
+	T0Count++;
+	if(T0Count>1000){
+		T0Count=0;
+		P2_0=~P2_0;
+	}
+	
 
+	
+	
 }
